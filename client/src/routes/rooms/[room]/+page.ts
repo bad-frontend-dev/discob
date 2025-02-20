c881631ad264d7e3247bee8418e885feb05b664c
cobdev3@proton.me <cobdev3@proton.me>
2025-02-20 17:33:15 +0000

    more placeholder rooms

--- client/src/routes/rooms/[room]/+page.ts
+++ client/src/routes/rooms/[room]/+page.ts
@@ -12,6 +12,26 @@ const testRooms: Room[] = [
 		name: "room2",
 		id: "reallycoolroom",
 	},
+	{
+		image: "https://cdn.discordapp.com/avatars/535321378291843074/8bfa4b4f795280b635b3fc633ba0a30d.webp",
+		name: "meow",
+		id: "meow",
+	},
+	{
+		image: "https://cdn.discordapp.com/avatars/535321378291843074/8bfa4b4f795280b635b3fc633ba0a30d.webp",
+		name: "waow",
+		id: "a",
+	},
+	{
+		image: "https://cdn.discordapp.com/avatars/535321378291843074/8bfa4b4f795280b635b3fc633ba0a30d.webp",
+		name: "room3",
+		id: "thatoneroom",
+	},
+	{
+		image: "https://cdn.discordapp.com/avatars/535321378291843074/8bfa4b4f795280b635b3fc633ba0a30d.webp",
+		name: "buh",
+		id: "dslkgasd",
+	},
 ];
 
 export const load: PageLoad = ({ params }) => {
